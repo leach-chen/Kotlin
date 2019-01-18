d0d84d32e236e30b5c1141b665e64edb5b1606cc
leach.chen <leach>
2019-01-18 10:41:47 +0800

    modify

--- Prj/TestKotlin/app/src/main/java/com/leachchen/testkotlin/MainActivity.kt
+++ Prj/TestKotlin/app/src/main/java/com/leachchen/testkotlin/MainActivity.kt
@@ -2,15 +2,19 @@ package com.leachchen.testkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val price = "${'$'}9.99"
+        Log.e("mytest",price)
+
+        var a  = "aa"
+
+        Log.e("mytest",a)
     }
 
-   fun test(){
-
-   }
 }
